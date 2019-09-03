00bcb649af7b91da001639edc5f8a06ab79b5620
Jimmi Setiawan <53130905+MiSetya@users.noreply.github.com>
2019-09-03 19:28:08 +0700

    Add files via upload

---
+++ MiSetya.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/bash
+# Coded by. MiSetya
+# Mau recode? ngotak goblok!
+
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+mer='\033[41;97m' #Tepi
+R='\x1b[1;31m'
+G='\x1b[1;32m'
+B='\x1b[1;34m'
+Y='\x1b[1;33m'
+C='\x1b[1;36m'
+D='\x1b[0m'
+endc='\E[0m'
+enda='\033[0m'
+
+echo
+clear
+echo
+echo $pu     "╔═══════════╗"
+echo $pu   "╔═╝███████████╚═╗"
+echo $pu  "╔╝███████████████╚╗" $pu"Welcome to MiSetya Hacking tools"
+echo $pu  "║█████████████████║"
+echo $pu  "║█████████████████║" $me"Author : MiSetya"
+echo $pu  "║█████████████████║" $pu"Team   : Light Cyber Indonesia"
+echo $pu  "║█╔█████████████╗█║" $me"Contact: 0823-8623-4828"
+echo $pu  "╚╦╝███▒▒███▒▒███╚╦╝" $pu"Youtube: https://youtube.com/MiSetya"
+echo $pu  "╔╝██▒▒▒▒███▒▒▒▒██╚╗" $me"Gihub  : https://github.com/MiSetya"
+echo $pu  "║██▒▒▒▒▒███▒▒▒▒▒██║"
+echo $pu  "║██▒▒▒▒█████▒▒▒▒██║"
+echo $pu  "╚╗███████████████╔╝" $i"Masukan"
+echo $pu  "╔═╬══╦╝██▒█▒██╚╦══╝.▒.." $i"Nama"
+echo $pu  "║█║══║█████████║　...▒." $i"Anda"
+echo $pu  "║█║══║█║██║██║█║　.▒.." $i"Dibawah"
+echo $pu  "║█║══╚═╩══╩╦═╩═╩═╦╗▒.．"
+echo $pu "╔╝█╚══╦═╦══╦╩═╦═╦═╩╝"
+echo $pu"╔╝█████║█║██║██║█║" $cy"THANKS FOR USING THIS TOOL"
+echo $pu"║██████║█████████║" $cy"THANKS FOR USING THIS TOOL"
+echo
+read -p " Your name pliss>> " name
+sleep 2
+clear
+sleep 1
+echo
+echo
+echo $pu      "╔═══════════╗"
+echo $pu    "╔═╝███████████╚═╗"
+echo $pu   "╔╝███████████████╚╗"
+echo $pu   "║█████████████████║" $pu"<!><><><<><><><><><><><><!>"
+echo $pu   "║█████████████████║" $pu"<!>       WELCOME       <!>"
+echo $pu   "║█████████████████║" $pu"<!>       $name         <!>"
+echo $pu   "║█╔█████████████╗█║" $pu"<!><><><><><><><><><><>><!>"
+echo $pu   "╚╦╝███▒▒███▒▒███╚╦╝"
+echo $pu   "╔╝██▒▒▒▒███▒▒▒▒██╚╗"
+echo $pu   "║██▒▒▒▒▒███▒▒▒▒▒██║"
+echo $pu   "║██▒▒▒▒█████▒▒▒▒██║" $i"Wait......"
+echo $pu   "╚╗███████████████╔╝" $i"Tunggu bentar....."
+echo $pu  "╔═╬══╦╝██▒█▒██╚╦══╝.▒.."
+echo $pu  "║█║══║█████████║　...▒."
+echo $pu  "║█║══║█║██║██║█║　.▒.."
+echo $pu  "║█║══╚═╩══╩╦═╩═╩═╦╗▒.．"
+echo $pu "╔╝█╚══╦═╦══╦╩═╦═╦═╩╝"
+echo $pu"╔╝█████║█║██║██║█║" $cy"GUNAKAN TOOL DENGAN BIJAK!!"
+echo $pu"║██████║█████████║" $cy"GUNAKAN TOOL DENGAN BIJAK!!"
+echo
+sleep 5
+sh run.sh
+fi
