55803dcb056463c2af947cbe66554893e252b02f
Jimmi Setiawan <53130905+MiSetya@users.noreply.github.com>
2019-09-10 19:05:17 +0700

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,225 @@
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+mer='\033[41;97m' #Tepi
+R='\x1b[1;31m'
+G='\x1b[1;32m'
+B='\x1b[1;34m'
+Y='\x1b[1;33m'
+C='\x1b[1;36m'
+D='\x1b[0m'
+endc='\E[0m'
+enda='\033[0m'
+
+echo
+echo $pu"║" $pu"╔═══•ೋೋ•═══╗"
+echo $pu"║" $cy"MiSetya"
+echo $pu"║" $pu"╚═══•ೋೋ•═══╝"
+echo $pu"║"
+echo $pu"║" $pu"╔═══•ೋೋ•═══╗"
+echo $pu"║" $cy"Hacking"
+echo $pu"║" $pu"╚═══•ೋೋ•═══╝"
+echo $pu"║"
+echo $pu"║" $pu"╔═══•ೋೋ•═══╗"
+echo $pu"║" $cy"Tool"
+echo $pu"║" $pu"╚═══•ೋೋ•═══╝"
+echo $pu"║"
+echo $pu"╚═══Masukan Nama Anda═══"
+read -p "║>> " user
+sleep 2
+clear
+echo
+echo $pu"██████▀▀▀░░░░▀▀▀██████" $pu"╔═══•ೋೋ•═══╗"
+echo $pu"███▀░░░░░░░░░░░░░░▀███" $cy "MiSetya"
+echo $pu"██│░░░░░░░░░░░░░░░░│██" $pu"╚═══•ೋೋ•═══╝"
+echo $pu"█▌│░░░░░░░░░░░░░░░░│▐█"
+echo $pu"█░└┐░░░░░░░░░░░░░░░┌┘█" $pu"╔═══•ೋೋ•═══╗"
+echo $pu"█░░└┐░░░░░░░░░░░░░┌┘░█" $cy "Hacking"
+echo $pu"█░░┌┘▄▄▄▄░░░░▄▄▄▄▄└┐░█" $pu"╚═══•ೋೋ•═══╝"
+echo $pu"█▌░│████▌░░░▐█████│░▐█"
+echo $pu"██░│▐█▀▀░░▄░░▀▀██▌│░██" $pu"╔═══•ೋೋ•═══╗"
+echo $pu"█▀─┘░░░░░▐█▌░░░░░░└─▀█" $cy "Tools"
+echo $pu"█▄░░▄▄▓░░▀█▀░░▓▄▄▄░░▄█" $pu"╚═══•ೋೋ•═══╝"
+echo $pu"█▄─┘██▌░░░░░░░░▐██└─▄█"
+echo $pu"██░░▐█─┬┬┬┬┬┬┬┬─█▌░░██" $me"Author : MiSetya"
+echo $pu"█▌░░░▀🦂┼🦂┼🦂┼🦂┼▀░▐█" $pu"Team   : Light Cyber Indonesia"
+echo $pu"██▄░░░└┴┴┴┴┴┴┴┴┘░░▄███" $me"Thanks : All Member L.C.I"
+echo $pu"████▄░░░░░░░░░░░░▄████" $pu"Contact: 0823-8623-4828"
+echo $pu"███████▄▄▄▄▄▄▄████████"
+echo
+sleep 2
+echo
+echo $pu"◆━━━◆♤◆━━━◆◆━━━◆♤◆━━━◆◆━━━◆♤◆━━━◆◆━━━◆♤"
+echo $pu"◆ $cy Welcome $user to MiSetya Hacking tools $pu ◆"
+echo $pu"◆━━━◆♤◆━━━◆◆━━━◆♤◆━━━◆◆━━━◆♤◆━━━◆◆━━━◆♤"
+sleep 3
+echo
+echo
+echo "$cy ╔═══•ೋೋ•═══╗"
+echo $cy"Waiting the tools"
+echo "$cy ╚═══•ೋೋ•═══╝"
+echo
+echo
+sleep 5
+echo $i"║"$pu"====================================="
+echo $i"║"$pu"1. Dark Facebook NEW"
+echo $i"║"$pu"====================================="
+echo $i"║"$pu"2. Spam SMS all type"
+echo $i"║"$pu"====================================="
+echo $i"║"$pu"3. Rusmana Tools (tanpa user & pass)"
+echo $i"║"$pu"====================================="
+echo $i"║"$pu"4. Join Grup Whatsapp"
+echo $i"║"$pu"====================================="
+echo $i"║"$pu"5. Phising All Game"
+echo $i"║"$pu"====================================="
+echo $i"║"$pu"6. Tombol Kanan Kiri Termux"
+echo $i"║"$pu"====================================="
+echo $i"║"$pu"7. Ddos Attack"
+echo $i"║"$pu"====================================="
+echo $i"║"$pu"8. Pulsa Free"
+echo $i"║"$pu"====================================="
+echo $i"║"$pu"9. Whatsaap Exploit (Sadap WA)"
+echo $i"║"$pu"====================================="
+echo $i"║"$pu"10. Khusus orang Gabut"
+echo $i"║"$pu"====================================="
+echo $i"║"$pu"11. Deface Tebas Index Webdav"
+echo $i"║"$pu"====================================="
+echo $i"║"$pu"12. Buat Para Bucin"
+echo $i"║"$pu"====================================="
+echo $i"║"$pu"13. Spam Call"
+echo $i"║"$pu"====================================="
+echo $i"║"$pu"14. Script Deface Create"
+echo $i"║"$pu"====================================="
+echo $i"║"$pu"15. Trojan Nomor Handphone"
+echo $i"║"$pu"====================================="
+echo $i"║"$pu"16. Sadap WA (V2)"
+echo $i"║"$pu"====================================="
+echo $i"║"
+echo $i"╚═══════════MakeYourChoice═════════╗"
+read -p "║════> " pil
+
+case $pil in
+
+# Dark Facebook NEW
+1) git clone https://github.com/MiSetya/FB-NEW
+cd FB-NEW
+python2 NEW.py
+
+;;
+
+# sdv spam
+2) git clone https://github.com/mrmsdv/sdvspam.git
+cd sdvspam
+sh sdv.sh
+
+;;
+
+# Rusmana
+3) git clone https://github.com/Rusmana-ID/rus
+cd rus
+sh install.sh
+
+;;
+
+#grup wa
+4) xdg-open https://chat.whatsapp.com/IGSuEa2DLSqKhhPC8sAY36
+
+;;
+
+#Phising
+5) git clone https://github.com/InYourG00D1/scPH1S1N9
+cd scPH1S1N9
+sh PhisingV2.sh
+
+;;
+
+#tombol spesial
+6) git clone https://github.com/MiSetya/tombol-special
+cd tombol-specil
+php Aktif.php
+
+;;
+
+#ddos
+7) git clone https://github.com/MiSetya/VA-DDOS
+cd VA-DDOS
+chmod 777 80juta.py
+python2 80juta.py
+
+;;
+
+#pulsa
+8) git clone https://github.com/MiSetya/PulsaGratis
+cd PulsaGratis
+sh Gratis.sh
+
+;;
+
+#wa exploit
+9) git clone https://github.com/MiSetya/WhatsExploit
+cd WhatsExploit
+chmod 777 start-ex.sh
+sh start-ex.sh
+
+;;
+
+#Gabut
+10) git clone https://github.com/MiSetya/Gabut
+cd Gabut
+sh KNTL.sh
+
+;;
+
+#tebas
+11) git clone https://github.com/MiSetya/TEBAS-INDEX
+cd TEBAS-INDEX
+sh TEBAS.sh
+
+;;
+
+#bucin
+12) git clone https://github.com/MiSetya/BUCIN
+cd BUCIN
+sh BUCIN.sh
+
+;;
+
+#call
+13) git clone https://github.com/MiSetya/CALL
+cd CALL
+php telpon.php
+
+;;
+
+#script depes creator
+14) git clone https://github.com/MiSetya/script-depes
+cd script-depes
+chmod +x depes.py
+python2 depes.py
+
+;;
+
+#trojan
+15) git clone https://github.com/MiSetya/Trojans
+cd Trojans
+python2 nyerang.py
+
+;;
+
+#v2
+16) git clone https://github.com/MiSetya/SadapWA-V2
+cd SadapWA-V2
+chmod 777 SadapWA.sh
+sh SadapWA.sh
+
+;;
+
+*) echo "◆━━━◆♤◆━━━◆◆━━━◆♤◆━━━◆"
+echo "Thanks for using"
+echo "◆━━━◆♤◆━━━◆◆━━━◆♤◆━━━◆"
+esac
